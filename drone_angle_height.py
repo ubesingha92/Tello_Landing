99821406836c7dfbfb5993c778105d9c2d740af6
Chanaka Chathuranga Ubesingha <ubesingha92@gmail.com>
2023-06-25 17:39:57 +0530

    new update angle and height file

---
+++ drone_angle_height.py
@@ -0,0 +1,27 @@
+import math
+from djitellopy import Tello
+import time
+
+# Initialize Tello drone
+tello = Tello()
+
+# Connect to the drone
+tello.connect()
+
+try:
+    while True:
+        # Get the roll and pitch angles in degrees and convert degrees to radians
+        roll = math.radians(tello.get_roll())
+        pitch = math.radians(tello.get_pitch())
+
+        # Distance height between the drone and the ArUco marker
+        height = tello.get_distance_tof() * math.cos(roll) * math.cos(pitch)
+
+        print("Height: ", height)
+        time.sleep(0.2)
+
+except KeyboardInterrupt:
+    print("Keyboard interrupt detected. Stopping the program.")
+
+# Disconnect from the Tello
+tello.disconnect()
