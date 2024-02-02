07df24c926adddf08451037d628acb35f6e3577c
Chanaka Chathuranga Ubesingha <ubesingha92@gmail.com>
2024-02-02 23:33:32 +0530

    PID Test Start

--- Drone_Land_SPID_Test.py
+++ Drone_Land_SPID_Test.py
@@ -3,7 +3,10 @@ import numpy as np
 from djitellopy import Tello
 import math
 import time
+import datetime
 from simple_pid import PID
+import csv
+import os
 
 # Define frame size
 FRAME_SIZE = (320, 240)
@@ -14,34 +17,39 @@ calib_data_path = "Camera_Calibartion/calib.npz"
 # Size of the ArUco markers (in centimeters)
 MARKER_SIZE = 7.5  # cm
 
+# Define the sample time
+SAMPLE_TIME = 0.4  # seconds
+
 # PID coefficients for X
 # KP_X, KI_X, KD_X = 3, 0, 5
 KP_X, KI_X, KD_X = 0, 0, 0
 MAX_SPEED_X = 15
-pid_x = PID(KP_X, KI_X, KD_X, setpoint=0)
+pid_x = PID(KP_X, KI_X, KD_X, setpoint=0, sample_time = SAMPLE_TIME)
 pid_x.output_limits = (-MAX_SPEED_X, MAX_SPEED_X)
 
 # PID coefficients for Y
 # KP_Y, KI_Y, KD_Y = 3, 0, 5
 KP_Y, KI_Y, KD_Y = 0, 0, 0
 MAX_SPEED_Y = 15
-pid_y = PID(KP_Y, KI_Y, KD_Y, setpoint=0)
+pid_y = PID(KP_Y, KI_Y, KD_Y, setpoint=0, sample_time = SAMPLE_TIME)
 pid_y.output_limits = (-MAX_SPEED_Y, MAX_SPEED_Y)
 
 # PID coefficients for yaw
 # KP_YAW, KI_YAW, KD_YAW = 4, 0.01, 1
 KP_YAW, KI_YAW, KD_YAW = 4, 0.01, 1
 MAX_SPEED_YAW = 15
-pid_yaw = PID(KP_YAW, KI_YAW, KD_YAW, setpoint=0)
+pid_yaw = PID(KP_YAW, KI_YAW, KD_YAW, setpoint=0, sample_time = SAMPLE_TIME)
 pid_yaw.output_limits = (-MAX_SPEED_YAW, MAX_SPEED_YAW)
 
 XY_ACCEPT_RANGE = 3
 YAW_ACCEPT_RANGE = 3
-OK_COUNTER_LIMIT = 1
 
 # Connect to Tello drone
 tello = Tello()
 tello.connect()
+
+filename_time = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+csv_filename = f'csv/aruco_marker_errors_{filename_time}.csv'
 
 def load_calibration_data(path):
     # Load calibration data
@@ -52,15 +60,25 @@ def load_calibration_data(path):
 
     return cam_mat, dist_coef
 
-def draw_detected_markers(frame, corners, ids, rvecs, tvecs):
-    global ok_counter
+def write_error_to_csv(file_path, error_data):
+    # Column titles
+    column_titles = ['Time', 'Marker ID', 'Error X', 'Error Y', 'Yaw Error', 'Height', 'PID Value X', 'PID Value Y', 'PID Value Yaw']
+    
+    # Check if the file exists and is empty
+    file_exists = os.path.isfile(file_path)
+    write_header = not file_exists or os.stat(file_path).st_size == 0
+    
+    with open(file_path, mode='a', newline='') as file:
+        writer = csv.writer(file)
+        
+        # If the file is new or empty, write the column titles first
+        if write_header:
+            writer.writerow(column_titles)
+        
+        # Then write the actual data
+        writer.writerow(error_data)
 
-    # Convert degrees to radians
-    roll = math.radians(tello.get_roll())
-    pitch = math.radians(tello.get_pitch())
-
-    # Distance height between the drone and the ArUco marker
-    height = tello.get_distance_tof() * math.cos(roll) * math.cos(pitch)
+def control_markers(frame, corners, ids, rvecs, tvecs, height, roll, pitch):
 
     for i in range(len(ids)):
         cv2.aruco.drawDetectedMarkers(frame, corners)
@@ -68,8 +86,8 @@ def draw_detected_markers(frame, corners, ids, rvecs, tvecs):
         x, y, _ = [int(round(item)) for sublist in tvecs[i] for item in sublist]  # Unpack and round tvecs[i]
 
         # Correction
-        x = -x + height * math.sin(roll)
-        y = y - height * math.sin(pitch)
+        error_x = -x + int(height * math.sin(roll))
+        error_y = y - int(height * math.sin(pitch))
 
         # Calculate Euler angles
         rotation_matrix, _ = cv2.Rodrigues(rvecs[i])
@@ -77,35 +95,39 @@ def draw_detected_markers(frame, corners, ids, rvecs, tvecs):
 
         # Round angles and convert to integer (Roll, Pitch, Yaw)
         euler_angles = [int(round(angle[0])) for angle in euler_angles]
-        yaw_value = euler_angles[2]
+        yaw_error = euler_angles[2]
 
         # Get Battery
         battery_value = tello.get_battery()
 
         # PID Control
-        x_adjustment = int(pid_x(x))
-        y_adjustment = int(pid_y(y))
-        yaw_adjustment = int(pid_yaw(yaw_value))
+        pid_val_x = int(pid_x(error_x))
+        pid_val_y = int(pid_y(error_y))
+        pid_val_z = 0
+        pid_val_yaw = int(pid_yaw(yaw_error))
 
-        print (f"Battery: {battery_value} X: {x_adjustment} Y: {y_adjustment} Yaw: {yaw_adjustment}", end="")
+        print (f"Battery: {battery_value} X: {error_x} Y: {error_y} Yaw: {yaw_error} Height: {height}", end="")
 
         # Tello movement control based on PID adjustments
-        if -YAW_ACCEPT_RANGE > yaw_value or yaw_value > YAW_ACCEPT_RANGE:
-            tello.send_rc_control(0, 0, 0, yaw_adjustment)
+        if -YAW_ACCEPT_RANGE > yaw_error or yaw_error > YAW_ACCEPT_RANGE:
+            pid_val_x = 0
+            pid_val_y = 0
             print ("--Need Yaw Correction")
-            ok_counter = 0
-        elif (-XY_ACCEPT_RANGE > x or x > XY_ACCEPT_RANGE) and (-XY_ACCEPT_RANGE > y or y > XY_ACCEPT_RANGE):
-            tello.send_rc_control(x_adjustment, y_adjustment, 0, yaw_adjustment)
+
+        elif (-XY_ACCEPT_RANGE > error_x or error_x > XY_ACCEPT_RANGE) and (-XY_ACCEPT_RANGE > error_y or error_y > XY_ACCEPT_RANGE):
             print ("--Need XY Correction")
-            ok_counter = 0
+
         else:
             print ("--Ok")
-            tello.send_rc_control(x_adjustment, y_adjustment, 0, yaw_adjustment)
-            ok_counter += 1
-            if ok_counter >= OK_COUNTER_LIMIT:
-                tello.send_rc_control(x_adjustment, y_adjustment, -15, yaw_adjustment)
-                if height <= 35:
-                    tello.land()
+            # tello.land()
+
+        # Write errors to CSV
+        human_readable_time = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
+        error_data = [human_readable_time, ids[i][0], error_x, error_y, yaw_error, height, pid_val_x, pid_val_y, pid_val_yaw]
+        write_error_to_csv(csv_filename, error_data)
+
+        # RC Control
+        # tello.send_rc_control(pid_val_x, pid_val_y, pid_val_z, pid_val_yaw)
 
 def process_frame(tello, frame_size):
     # Read frame from the drone
@@ -115,7 +137,7 @@ def process_frame(tello, frame_size):
     frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
     frame = frame[frame.shape[0] - frame_size[0]:frame.shape[0],
             frame.shape[1] - frame_size[1]:frame.shape[1]]
-
+    
     return frame
 
 def main():
@@ -130,14 +152,21 @@ def main():
     tello.set_video_direction(1)
 
     # takeoff
-    tello.takeoff()
+    # tello.takeoff()
 
     while True:
-        time.sleep(0.2)
+        time.sleep(SAMPLE_TIME)
         frame = process_frame(tello, FRAME_SIZE)
 
         # Detect ArUco markers
-        corners, ids, _ = detector.detectMarkers(frame)
+        corners, ids, _ = detector.detectMarkers(frame, cam_mat, dist_coef)
+
+        # Convert degrees to radians
+        roll = math.radians(tello.get_roll())
+        pitch = math.radians(tello.get_pitch())
+
+        # Distance height between the drone and the ArUco marker
+        height = int(tello.get_distance_tof() * math.cos(roll) * math.cos(pitch)) 
 
         # If any markers are detected
         if ids is not None:
@@ -147,16 +176,13 @@ def main():
             except Exception as e:
                 print(f"Error estimating marker pose: {e}")
 
-            draw_detected_markers(frame, corners, ids, rvecs, tvecs)
+            control_markers(frame, corners, ids, rvecs, tvecs, height, roll, pitch)
         else:
             # Handle no markers detected
-            roll = math.radians(tello.get_roll())
-            pitch = math.radians(tello.get_pitch())
-            height = tello.get_distance_tof() * math.cos(roll) * math.cos(pitch)
-            if height < 100:
+            if height < 150:
                 tello.send_rc_control(0, 0, 10, 0)
             else:
-                tello.send_rc_control(0, 0, 0, 0)
+                tello.land()
 
         cv2.imshow("frame", frame)
 
